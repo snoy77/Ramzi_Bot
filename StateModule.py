a306bc9cd7220e1288eff60b21db7d6929437970
snoy77 <snoy77@mail.ru>
2023-05-01 03:42:24 +0500

    fix arch

--- StateModule.py
+++ StateModule.py
@@ -1,109 +1,75 @@
+#Список ошибок:
+#   001 - неопределённый статус
+
 import DataBaseWorker as DBW
 
-WordsForState_0 = [ 'отмена', 'нет']
-StateForState_0 = [1, 5]
+#=============== States ==================
+Normal = 0
+AddIngridients = 1
+AddDishes = 2
+DishesIngridients = 3
 
-WordsForState_1 = [
+State = Normal
+
+#=============== Other ==================
+WordsForState_Normal = [ 'отмена', 'нет']
+WordsForState_AddIngridients = [
 'добавить продукт',
 'новый продукт',
 'нет',
 'да'
 ]
-StateForState_1 = [0, 1]
-StateForState_3 = [3]
+WordsForState_AddDishes = ['добавить блюдо']
+WordsForState_DishesIngridients = ['блюдо']
 
-WordsForState_5 = ['блюдо']
-StateForState_5 = [0]
-StateForState_6 = [5]
-BotState = 0
+#=============== Automat ================
+def StateWork(message):
+    global State
+    message_text = message.lower()
 
-#====== Check state ===================
-def CheckState_0(message_text, BotStateNow):
-    return message_text in WordsForState_0 and BotStateNow in StateForState_0
-def CheckState_1(message_text, BotStateNow):
-    return message_text in WordsForState_1 and BotStateNow in StateForState_1
-def CheckState_3(message_text, BotStateNow):
-    return BotStateNow == 1 and (message_text != 'да' or message_text != 'нет')
-def CheckState_5(message_text, BotStateNow):
-    return message_text in WordsForState_5 and BotStateNow in StateForState_5
-def CheckState_6(message_text, BotStateNow):
-    return BotStateNow == 5 and (message_text != 'да' or message_text != 'нет')
-
-def CheckState(message_text):
-    global BotState
-    if CheckState_0(message_text, BotState):
-        return 0
-    elif CheckState_1(message_text, BotState):
-        return 1
-    elif CheckState_3(message_text, BotState):
-        return 3
-    elif CheckState_5(message_text, BotState):
-        return 5
-    elif CheckState_6(message_text, BotState):
-        return 6
-    else:
-        return BotState
-
-#=========== Do state ================
-def doState_0(message_text):
-    return 'Ожидаю команды...'
-def doState_1(message_text):
-    return 'Назови продукт, пожалуйста'
-def doState_3(message_text):
-    DBW.return_query_result(query_add_ingredients(), (message_text,))
-    ChangeBotState(1)
-    return 'Новый продукт добавлен, добавить ещё?'
-def doState_5(message_text):
-    return 'Напиши интересующий тебя ингридиент'
-def doState_6(message_text):
-    args = message_text.split(',')
-    args = message_text.split(';')
-    result = DBW.return_query_result(query_get_dishes(args))
-    num = 1
-    resultMessage = ''
-    for element in result:
-        resultMessage += str(num) + '. ' + str(element[0]) + '\n'
-
-    ChangeBotState(5)
-    return resultMessage
-
-
-def doState(message_text):
-    global BotState
-    if BotState == 0:
-        return doState_0(message_text)
-    elif BotState == 1:
-        return doState_1(message_text)
-    elif BotState == 3:
-        return doState_3(message_text)
-    elif BotState == 5:
-        return doState_5(message_text)
-    elif BotState == 6:
-        return doState_6(message_text)
-
-
-#========================
-def ChangeBotState(newState):
-    global BotState
-    if BotState == newState:
-        return
-
-    message = 'Статус бота изменился с [' + str(BotState) +'] '
-    BotState = newState
-    message += 'на [' + str(BotState) + ']'
-    if BotState == newState:
-        print(message)
-        return True
-    else:
-        print(message + ' !!! СТАТУС БОТА НЕ СООТВЕТСВУЕТ НОВОМУ СТАТУСУ ПРИ ИЗМЕНЕНИИ В ФУНКЦИИ \'ChangeBotState()\'')
-
+    if State == Normal:
+        if message_text in WordsForState_AddIngridients:
+            ChangeBotState(AddIngridients)
+            return 'Назови ингридиент'
+        elif message_text in WordsForState_AddDishes:
+            ChangeBotState(AddDishes)
+            return 'Назови блюдо'
+        elif message_text in WordsForState_DishesIngridients:
+            ChangeBotState(DishesIngridients)
+            return 'Напиши интересующий тебя ингридиент'
+        else:
+            return 'Неизвестная просьба'
+    elif State == AddIngridients:
+        if message_text in WordsForState_Normal:
+            ChangeBotState(Normal)
+            return "Буду ждать следующей команды"
+        DBW.return_query_result(query_add_ingredients(), (message_text,))
+        return 'Новый продукт добавлен, добавить ещё?'
+        pass
+    elif State == AddDishes:
+        if message_text in WordsForState_Normal:
+            ChangeBotState(Normal)
+            return "Буду ждать следующей команды"
+        pass
+    elif State == DishesIngridients:
+        if message_text in WordsForState_Normal:
+            ChangeBotState(Normal)
+            return "Буду ждать следующей команды"
+        args = message_text.split(',')
+        args = message_text.split(';')
+        result = DBW.return_query_result(query_get_dishes(args))
+        num = 1
+        resultMessage = ''
+        for element in result:
+            resultMessage += str(num) + '. ' + str(element[0]) + '\n'
+        return resultMessage + "\nНайти блюда для ещё одного ингридиента?"
+    return "ОШИБКА №001.\nОбратитесь к разработчикам"
 
 #====== ЗАПРОСЫ ==========
 def query_add_ingredients():
     query_to_bd = "insert into ingredients (name) values (%s)"
     return query_to_bd
 def query_get_dishes(args):
-
     query_to_bd = "CREATE TEMPORARY TABLE `need_ingredients` SELECT id FROM `ingredients` WHERE name IN ("
     query_to_bd += "\'\'"
     for el in args:
@@ -111,3 +77,18 @@ def query_get_dishes(args):
 
     query_to_bd += "); CREATE TEMPORARY TABLE `TT_dishes` SELECT dishes_ingredients.id_dishes FROM `dishes_ingredients` INNER JOIN `need_ingredients` ON dishes_ingredients.id_ingredients = need_ingredients.id; SELECT dishes.name FROM `dishes` INNER JOIN `TT_dishes` ON dishes.id = TT_dishes.id_dishes"
     return query_to_bd.replace('`', '')
+
+#=========== вспомогательные процедуры и функции ========================
+def ChangeBotState(newState):
+    global State
+    if State == newState:
+        return
+
+    message = 'Статус бота изменился с [' + str(State) +'] '
+    State = newState
+    message += 'на [' + str(State) + ']'
+    if State == newState:
+        print(message)
+        return True
+    else:
+        print(message + ' !!! СТАТУС БОТА НЕ СООТВЕТСВУЕТ НОВОМУ СТАТУСУ ПРИ ИЗМЕНЕНИИ В ФУНКЦИИ \'ChangeBotState()\'')
--- main.py
+++ main.py
@@ -4,22 +4,13 @@ from telebot import types
 import TelegaInfo as TI
 import StateModule as SM
 
-#Статус нынешней операции
-#   *0 - начало работы, ожидание любого события
-#   *1 - Ожидание названия материала для добавления его в БД
-#   2 - Ожидание названия материала для удаления его в БД
-#   *3 - Добавление продукта в список БД
-#   4
-#   5 - ожидание навзаний ингридиентов для получения имеющихся рецептов
-#   6 - дать рецепты
-
-
-
 #----------------Определение некоторых методов----------------
 def getMessageChatMainInfo(chat):
     return str(chat.id) + ' ' + str(chat.username) + ' ' + str(chat.title)
 def getMessageUserMainInfo(user):
     return str(user.id) + ' ' + str(user.first_name) + ' ' + str(user.last_name)
+
+
 
 
 
@@ -36,12 +27,17 @@ def message_going(message):
     message_user = getMessageUserMainInfo(message.from_user)
     message_chat = getMessageChatMainInfo(message.chat)
 
-    BotState_Need = SM.CheckState(message_text)
-    SM.ChangeBotState(BotState_Need)
-    going_message = SM.doState(message_text)
+    #BotState_Need = SM.CheckState(message_text)
+    #SM.ChangeBotState(BotState_Need)
+    #going_message = SM.doState(message_text)
+    going_message = SM.StateWork(message_text)
 
     bot.send_message(chat_id, going_message)
 
-print('Работа бота...\n')
-bot.polling(none_stop=True, interval=0)
-print('\n\nРабота бота ЗАВЕРШЕНА')
+while True:
+    print('Работа бота...\n')
+    try:
+        bot.polling(none_stop=True, interval=0)
+    except ZeroDivisionError as err:
+        print('\n\nАварийное выключение бота:\n\n')
+    print('\n\nРабота бота ЗАВЕРШЕНА')
